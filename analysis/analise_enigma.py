c0f799cfb36096deb70d020eb3b4f3aed3b076e3
Pinkthepink <michelnikolaides@gmail.com>
2022-11-14 18:44:53 -0300

    jkjkjkkjk

--- analysis/analise_enigma.py
+++ analysis/analise_enigma.py
@@ -1,8 +1,32 @@
 from analysis.fitness.ioc import faz_ioc
 
-def acha_configuracao_rotor (texto_cifrado, chave_necessaria, rotores):
-    rotores_disponiveis = [1,2,3,4,5]
+class analise_enigma:
 
-    rotores_otimizados = []
-    posicoes_otimizadas = []
-    
+    def acha_configuracao_rotor (texto_cifrado, chave_necessaria, plugboard, funcao_fitness):
+        rotores_disponiveis = ["I","II","III","IV","V"]
+
+        rotores_otimizados = []
+        posicoes_otimizadas = []
+
+        conjunto_de_chaves = []
+        
+        for rotor1 in rotores_disponiveis:
+            for rotor2 in rotores_disponiveis:
+                if rotor1 == rotor2: continue 
+                for rotor3 in rotores_disponiveis:
+                    if rotor1 == rotor3 or rotor2 == rotor3: continue
+                    print(rotor1,"",rotor2,"",rotor3)
+
+                    maximo_fitness = 10**(-30)
+                    chave_enigma = None
+                    for i in range(0,26,1):
+                        for j in range(0,26,1):
+                            for k in range(0,26,1):
+                                e = Enigma([rotor1,rotor2,rotor3],"B",[i,j,k], [0,0,0], plugboard) #importar quando estiver pronto
+                                decriptar = (texto_cifrado)
+                                
+
+
+
+                
+ 
--- analysis/chave_enigma.py
+++ analysis/chave_enigma.py
@@ -6,13 +6,13 @@ class chave_enigma:
 
     def __init__(self, coringa, indicadores = None, posicao_inicial_rotores = None, plugboard_conections = None):
         if isinstance(coringa, chave_enigma): 
-            self.rotores = coringa.rotores if coringa.rotores != None else [1,2,3]
+            self.rotores = coringa.rotores if coringa.rotores != None else ["I","II","III"]
             self.indicadores = coringa.indicadores if coringa.indicadores != None else [0,0,0]
             self.posicao_inicial_rotores = coringa.posicao_inicial_rotores if coringa.posicao_inicial_rotores != None else [0,0,0]
             self.plugboard = coringa.plugboard if coringa.plugboard != None else ""
     
         elif isinstance(coringa, list):
-            self.rotores = coringa if coringa != None else [1,2,3]
+            self.rotores = coringa if coringa != None else ["I","II","III"]
             self.indicadores = indicadores if indicadores != None else [0,0,0]
             self.posicao_inicial_rotores = posicao_inicial_rotores if posicao_inicial_rotores != None else [0,0,0]
             self.plugboard = plugboard_conections if plugboard_conections != None else ""
