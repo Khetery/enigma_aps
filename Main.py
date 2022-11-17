b39d6a91bf50c61f7ae35d90f5ef3b4e2130784e
Pinkthepink <michelnikolaides@gmail.com>
2022-11-17 14:43:33 -0300

    lo ura

--- Main.py
+++ Main.py
@@ -1,7 +1,23 @@
 from enigma.Enigma import Enigma
 
-maquinaenigma = Enigma(["I","IV","V"],"C",[8,6,13],[1,3,5],None )
+rotor1 = input("Qual é o primeiro rotor?(I-V)")
+rotor2 = input("Qual é o segundo rotor?(I-V)")
+rotor3 = input("Qual é o terceiro rotor?(I-V)")
+espelho = input("O modelo do refletor é B ou C?")
 
-texto = maquinaenigma.encripta("NeuralnetworksinparticularrecurrentneuralnetworksRNNsarenowatthecoreoftheleadingapproachestolanguageunderstandingtaskssuchaslanguagemodelingmachinetranslationandquestionansweringInAttentionIsAllYouNeedweintroducetheTransformeranovelneuralnetworkarchitecturebasedonaselfattentionmechanismthatwebelievetobeparticularlywellsuitedforlanguageunderstandingInourpaperweshowthattheTransformeroutperformsbothrecurrentandconvolutionalmodelsonacademicEnglishtoGermanandEnglishtoFrenchtranslationbenchmarksOntopofhighertranslationqualitytheTransformerrequireslesscomputationtotrainandisamuchbetterfitformodernmachinelearninghardwarespeedinguptrainingbyuptoanorderofmagnitude")
+pos1 = int(input("Qual a posição inicial do primeiro rotor?(0-25)"))
+pos2 = int(input("Qual a posição inicial do segundo rotor?(0-25)"))
+pos3 = int(input("Qual a posição inicial do terceiro rotor?(0-25)"))
 
+anel1 = int(input("Qual deve ser a variação do output para o rotor1?(0-25)"))
+anel2 = int(input("Qual deve ser a variação do output para o rotor2?(0-25)"))
+anel3 = int(input("Qual deve ser a variação do output para o rotor3?(0-25)"))
+
+maquinaenigma = Enigma([rotor1,rotor2,rotor3],espelho,[pos1,pos2,pos3],[anel1,anel2,anel3],None)
+
+aserencriptado = input("máquina configurada, qual texto devemos transformar?")
+
+texto = maquinaenigma.encripta(aserencriptado)
+
+print("O texto resultante é :")
 print("".join(texto))
